a952531e4d3c262f022d9aa4d6bbc1703713ef0d
Michael Siehl <miesiehl@gmail.com>
2016-04-17 22:39:29 +0200

    Create Main.f90

---
+++ src/Main.f90
@@ -0,0 +1,13 @@
+! This file is part of:
+! MPMD with Coarray Fortran (2008): an Example Program (I)
+! copyright 2016 by Michael Siehl
+! www.mpmd-with-coarray-fortran.de
+! http://www.mpmd-with-coarray-fortran.de/MPMD_with_Coarray_Fortran_2008.pdf
+
+PROGRAM Main
+  !
+  USE Main_Sub
+  !
+  CALL Entry_Main_Sub ()
+  !
+END PROGRAM Main
