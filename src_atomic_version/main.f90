208ca7f4de4a2ab8e0e5dc7614b3f230c6bcf49a
Michael Siehl <miesiehl@gmail.com>
2016-10-23 17:59:09 +0200

    Create main.f90

---
+++ src_atomic_version/main.f90
@@ -0,0 +1,13 @@
+! This file is part of:
+! MPMD with Coarray Fortran Load Balancing – Example Program
+! copyright 2016 by Michael Siehl
+! www.mpmd-with-coarray-fortran.de
+! http://www.mpmd-with-coarray-fortran.de/MPMD_Load_Balancing_example.pdf
+
+PROGRAM Main
+  !
+  USE Main_Sub
+  !
+  CALL Entry_Main_Sub ()
+  !
+END PROGRAM Main
